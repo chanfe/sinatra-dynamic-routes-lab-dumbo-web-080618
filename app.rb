4b22b5083d6c4944463c0e79c388cc9379315d94
github username <github email address>
2018-08-27 16:53:24 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,48 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @user_name = params[:name]
+    "Hello #{@user_name.reverse}!"
+  end
 
+  get '/square/:number' do
+    @number = params[:number].to_f
+    "#{@number**2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    answer = ""
+    for i in 0..@number-1
+      answer << "#{@phrase}"
+    end
+    answer
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @number1 = params[:number1].to_f
+    @number2 = params[:number2].to_f
+    case params[:operation]
+    when "add"
+      "#{@number1 + @number2}"
+    when "subtract"
+      "#{@number1 - @number2}"
+    when "multiply"
+      "#{@number1 * @number2}"
+    when "divide"
+      "#{@number1 / @number2}"
+    end
+
+  end
 end
